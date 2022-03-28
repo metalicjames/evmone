3329a676046f73f6cf2f87d176ff519c097ea6a6
Paweł Bylica <chfast@gmail.com>
2022-03-28 21:52:41 +0200

    WIP EIP-1559 costs

--- test/state/state.cpp
+++ test/state/state.cpp
@@ -20,6 +20,17 @@ int64_t compute_tx_data_cost(evmc_revision rev, bytes_view data) noexcept
         cost += (b == 0) ? zero_byte_cost : nonzero_byte_cost;
     return cost;
 }
+
+int64_t compute_access_list_cost(const AccessList& access_list) noexcept
+{
+    static constexpr auto storage_key_cost = 1900;
+    static constexpr auto address_cost = 2400;
+
+    int64_t cost = 0;
+    for (const auto& a : access_list)
+        cost += address_cost + static_cast<int64_t>(a.second.size()) * storage_key_cost;
+    return cost;
+}
 }  // namespace
 
 void transition(State& state, const BlockInfo& block, const Tx& tx, evmc_revision rev, evmc::VM& vm)
@@ -35,23 +46,29 @@ void transition(State& state, const BlockInfo& block, const Tx& tx, evmc_revisio
     const auto value_be = intx::be::store<evmc::uint256be>(tx.value);
 
     const auto data_gas = compute_tx_data_cost(rev, tx.data);
+    const auto access_list_cost = compute_access_list_cost(tx.access_list);
+
+    const auto intrinsic_gas = tx.gas_limit + data_gas + access_list_cost + 21000;
+    assert(block.gas_limit >= intrinsic_gas);
+    assert(state.accounts[tx.sender].balance >= intrinsic_gas * tx.max_gas_price);
 
     evmc_message msg{EVMC_CALL, 0, 0, tx.gas_limit, tx.to, tx.sender, tx.data.data(),
         tx.data.size(), value_be, {}, tx.to};
     const auto gas_left = vm.execute(host, rev, msg, code.data(), code.size()).gas_left;
 
-    const auto intrinsic_gas = tx.gas_limit + data_gas + 21000;
-    assert(block.gas_limit >= intrinsic_gas);
-    assert(state.accounts[tx.sender].balance >= intrinsic_gas * tx.max_gas_price);
-
-    const auto gas_used = tx.gas_limit - gas_left + 21000 + data_gas;
-    const auto sender_fee = gas_used * tx.max_gas_price;
+    const auto gas_used = intrinsic_gas - gas_left;
 
     const auto base_fee = (rev >= EVMC_LONDON) ? block.base_fee : 0;
+
     assert(tx.max_gas_price >= base_fee);
-    const auto priority_fee = tx.max_gas_price - base_fee;
-    assert(priority_fee >= 0);
-    const auto producer_pay = gas_used * priority_fee;
+    assert(tx.max_gas_price >= tx.max_priority_gas_price);
+
+    const auto priority_gas_price =
+        std::min(tx.max_priority_gas_price, tx.max_gas_price - base_fee);
+    const auto effective_gas_price = base_fee + priority_gas_price;
+
+    const auto sender_fee = gas_used * effective_gas_price;
+    const auto producer_pay = gas_used * priority_gas_price;
 
     assert(state.accounts[tx.sender].balance >= sender_fee);
     state.accounts[tx.sender].balance -= sender_fee;
