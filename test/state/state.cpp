96ff9209dc3ce4bc305f1a876a247694b0ed3ea8
Paweł Bylica <chfast@gmail.com>
2022-03-28 12:08:08 +0200

    Apply tx data cost

--- test/state/state.cpp
+++ test/state/state.cpp
@@ -9,6 +9,19 @@
 
 namespace evmone::state
 {
+namespace
+{
+int64_t compute_tx_data_cost(evmc_revision rev, bytes_view data) noexcept
+{
+    constexpr int64_t zero_byte_cost = 4;
+    const int64_t nonzero_byte_cost = rev >= EVMC_ISTANBUL ? 16 : 68;
+    int64_t cost = 0;
+    for (const auto b : data)
+        cost += (b == 0) ? zero_byte_cost : nonzero_byte_cost;
+    return cost;
+}
+}  // namespace
+
 void transition(State& state, const BlockInfo& block, const Tx& tx, evmc_revision rev, evmc::VM& vm)
 {
     state.accounts[tx.sender].nonce += 1;
@@ -20,18 +33,23 @@ void transition(State& state, const BlockInfo& block, const Tx& tx, evmc_revisio
 
     bytes_view code = state.accounts[tx.to].code;
     const auto value_be = intx::be::store<evmc::uint256be>(tx.value);
+
+    const auto data_gas = compute_tx_data_cost(rev, tx.data);
+
     evmc_message msg{EVMC_CALL, 0, 0, tx.gas_limit, tx.to, tx.sender, tx.data.data(),
         tx.data.size(), value_be, {}, tx.to};
     const auto gas_left = vm.execute(host, rev, msg, code.data(), code.size()).gas_left;
 
-    const auto gas_used = tx.gas_limit - gas_left + 21000;
+    const auto gas_used = tx.gas_limit - gas_left + 21000 + data_gas;
     const auto sender_fee = gas_used * tx.gas_price;
 
     const auto base_fee = (rev >= EVMC_LONDON) ? block.base_fee : 0;
     assert(tx.gas_price >= base_fee);
     const auto priority_fee = tx.gas_price - base_fee;
+    assert(priority_fee >= 0);
     const auto producer_pay = gas_used * priority_fee;
 
+    assert(state.accounts[tx.sender].balance >= sender_fee);
     state.accounts[tx.sender].balance -= sender_fee;
     state.accounts[block.coinbase].balance += producer_pay;
 }
